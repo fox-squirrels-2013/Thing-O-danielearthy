f86d62c3636d9f18fc1a3ce81fb01f234843a4ad
Daniel Earthy <danielearthy@gmail.com>
2013-10-28 12:28:56 -0700

    First Commit

---
+++ myapp.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  'This Thing-o app will be about airplanes, specifically a fleet management tool for flight schools.
+   I intend to model the relationship between pilots and planes they use.  This will keep track of schedule for planes
+  as they relate to pilots as they rent planes either inividually or with an instructor, 
+  as well as keeping track of planes maintenance schedules with plans to roll out features to map individual part life.'
+end
